f54aea220b56ede020ebd43f920ca1841711fc89
kamalesh M <kamaleshmurugeshan@gmail.com>
2024-04-24 18:26:09 +0530

    CHANGED THE ERROR

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 import CustomerAdd from "./CusomerAdd";
-
+import Fetch from "./Fetch";
+import Axios from "./Axios";
+import UseFetch from "./UseFetch";
+import Query from "./Query";
+import Swr from "./Swr";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import CustomerView from "./CustomerView";
@@ -11,17 +15,17 @@ function App() {
             <div className="App">
                 <CustomerAdd />
                 <CustomerView />
-                {/* <Fetch />
-            <hr />
-            <Axios />
-            <hr />
-            <UseFetch />
-            <hr />
+                <Fetch />
+                <hr />
+                <Axios />
+                <hr />
+                <UseFetch />
+                <hr />
 
-            <Query />
-            <hr />
-            <Swr />
-            <hr /> */}
+                <Query />
+                <hr />
+                <Swr />
+                <hr />
             </div>
         </Provider>
     );
